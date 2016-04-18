806c50bfb512a5f7717e7a84da2aec14458b9bf1
Dan Heintzelman & Ian Wudarski <pair+heintzdm.ianwaterski@devbootcamp.com>
2016-04-18 17:24:22 -0500

    fix file structure and add validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :movie
 
+  validates_presence_of :rating
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :review
 
-
+  validates_presence_of :user, :review, :helpful
 end
