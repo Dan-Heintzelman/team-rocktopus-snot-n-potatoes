32f190eb1a31ca394b01cf6e0ca8cd5d10a2794b
Luis Ybarra & Taylor Daugherty <pair+lrybarra.taylordaug@devbootcamp.com>
2016-04-19 17:37:03 -0500

    yooooo

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,7 @@ module MoviesHelper
     genres = ["Comedy", "Action", "Romance", "Drama", "Horror", "Adventure", "Animation", "Crime", "Fantasy", "Mystery", "Western", "Thriller"]
     genres.sample(1)
   end
+
+  def imdb_ish
+  end
 end
