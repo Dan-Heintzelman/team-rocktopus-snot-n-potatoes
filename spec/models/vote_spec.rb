abfb0e4d478d69303d86a292f838da1d8b05f378
Dan Heintzelman & Ian Wudarski <pair+heintzdm.ianwaterski@devbootcamp.com>
2016-04-18 19:17:06 -0500

    fix tests

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,19 +1,6 @@
 describe Vote do
 
-  it "has no votes at instantiation" do
-    expect(Vote.count).to eq(0)
-  end
+  # number of times helpful shows up in database
 
-  it "has a vote in the database after creation" do
-
-    # where is a vote supposed to live? not sure how to test this
-
-    # dan = User.create(username: 'dan', email: 'dan@dan.com')
-    # review = Review.create(user: dan, rating: 8)
-    # # movie = Movie.create(title: 'Star Wars')
-    #
-    # Vote.create(user: dan, review: review)
-    #
-    # expect(vote.count).to eq(1)
   end
 end
