44b4a6d747cc2234bd6660b754dce5ad3c61f7a3
Dan Heintzelman & Ian Wudarski <pair+heintzdm.ianwaterski@devbootcamp.com>
2016-04-19 10:05:29 -0500

    controller testing and private methods

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -6,10 +6,20 @@ describe UsersController do
   describe "POST #create" do
     context "when valid user data is passed" do
       it "creates a new user" do
-        post(:create, {user:(username: 'ben', email: 'ben@ben.com', password: '123')})
+        post(:create, {user: { username: 'ben', email: 'ben@ben.com', password: '123'} })
         expect(assigns(:user)).to be_instance_of(User)
       end
     end
 
+    context "when invalid data is passed" do
+      
+    end
+  end
+
+  describe "GET #new" do
+    it "it renders the new page" do
+      get :new
+      expect(response).to render_template(:new)
+    end
   end
 end
